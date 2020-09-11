5d300034988682e3961ff68dac9ae46aa6425948
saurav <saurav.sharma.zz45@gmail.com>
2020-09-11 13:18:35 +0530

    added computer score
    new computer score feature has been added to the program so that gamer can see his score as compared to computer score

--- main.py
+++ main.py
@@ -6,55 +6,76 @@ import random
 pc_choice_rand = ["snake","water","gun"]
 def gamer_choose_snake():
     global score
+    global comp_score
     print("you had choosed Snake 🐍")
     print("Please wait ! computer is thinking..........!")
     comp_choice = random.choice(pc_choice_rand)
     if comp_choice == "snake":
         print("DRAW ! Snake can't fight with Snake")
         score = score+0
-        print("Your score is",score)
+        comp_score = comp_score+0
+        print("Your score🙋 is ",score)
+        print("The score🙋 of computer is :",comp_score)
     elif comp_choice == "water":
         print("YOU WIN !.. snake drinked the water")
         score = score+5
-        print("Your score is",score)
+        comp_score = comp_score-1
+        print("Your score🙋 is ",score)
+        print("The score🙋 of computer is :",comp_score)
     elif comp_choice == "gun":
         print("YOU LOOSE ! computer killed your snake by his Gun")
         score = score-1
-        print("Your score is",score)
+        comp_score = comp_score+5
+        print("Your score🙋 is",score)
+        print("The score🙋 of computer is :",comp_score)
 def gamer_choose_water():
     global score
+    global comp_score
     print("you had choosed Water 🌊")
     print("Please wait ! computer is thinking..........!")
     comp_choice = random.choice(pc_choice_rand)
     if comp_choice == "snake":
         print("YOU LOSE!.. snake drinked the water")
         score = score-1
-        print("Your score is",score)
+        comp_score = comp_score+5
+        print("Your score🙋 is",score)
+        print("The score🙋 of computer is :",comp_score)
     elif comp_choice == "water":
         print("DRAW ! Water can't fight with Water")
         score = score+0
-        print("Your score is",score)
+        comp_score = comp_score+0
+        print("Your score🙋 is",score)
+        print("The score🙋 of computer is :",comp_score)
     elif comp_choice == "gun":
         print("YOU WIN ! you destroyed the computer's Gun")
         score = score+5
-        print("Your score is",score)
+        comp_score = comp_score-1
+        print("Your score🙋 is",score)
+        print("The score🙋 of computer is :",comp_score)
 def gamer_choose_gun():
     global score
+    global comp_score
     print("you had choosed Gun 🔫")
     print("Please wait ! computer is thinking..........!")
     comp_choice = random.choice(pc_choice_rand)
     if comp_choice == "snake":
         print("YOU WIN ! you killed computer's snake by Gun ")
         score = score+5
-        print("Your score is",score)
+        comp_score = comp_score-1
+        print("Your score🙋 is",score)
+        print("The score🙋 of computer is :",comp_score)
     elif comp_choice == "water":
         print("YOU LOSE !.. computer's water wiped your gun away")
         score = score-1
-        print("Your score is",score)
+        comp_score = comp_score+5
+        print("Your score🙋 is",score)
+        print("The score🙋 of computer is :",comp_score)
     elif comp_choice == "gun":
         print("DRAW ! Gun can't fight with Gun")
         score = score+0
-        print("Your score is",score)
+        comp_score = comp_score+0
+        print("Your score🙋 is",score)
+        print("The score🙋 of computer is :",comp_score)
 
 
 # Main program here :
@@ -62,6 +83,7 @@ def gamer_choose_gun():
 print("==================== [[ SNAKE - WATER - GUN ]] =======================")
 usr_choice = input(str("you have 3 choices available......\n==> press s for snake 🐍\n==> press w for water🌊\n==> press g for gun🔫\n==> press e for exit⏱ the game and show score\n==> press any key to start the game\n"))
 score = 0
+comp_score = 0
 while usr_choice != "e":
     gamer_choice = str(input(("\nselect your choice between snake,water & gun or press e to exit the game\n")))
     if gamer_choice=="s":
@@ -75,7 +97,11 @@ while usr_choice != "e":
         if ex_conf == "n":
             continue
         else:
-            print("\n🐻__Thanks for your time__🐻\nexiting the game..⏱\n\nYour score 🙌 is :",score)
+            print("\n🐻__Thanks for your time__🐻\nexiting the game..⏱\n\nYour score 🙌 is :",score,"      computer score 🙌 is :",comp_score)
+            if score<comp_score:
+                print("The computer defeated you by ",comp_score - score,"points")
+            else:
+                print("You defeated the computer by",score - comp_score,"points")
             exit()
     else:
         print("Please choose between the provided options only 😕")
