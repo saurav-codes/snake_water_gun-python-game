ece144df56b376e7ae691d8782990a39a6947f01
saurav <saurav.sharma.zz45@gmail.com>
2020-09-11 10:58:43 +0530

    added some Emoji

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 pc_choice_rand = ["snake","water","gun"]
 
 def gamer_choose_snake():
-    print("you had choosed Snake")
+    print("you had choosed Snake 🐍")
     print("Please wait ! computer is thinking..........!")
     comp_choice = random.choice(pc_choice_rand)
     if comp_choice == "snake":
@@ -16,7 +16,7 @@ def gamer_choose_snake():
     elif comp_choice == "gun":
         print("YOU LOOSE ! computer killed your snake by his Gun")
 def gamer_choose_water():
-    print("you had choosed Water")
+    print("you had choosed Water 🌊")
     print("Please wait ! computer is thinking..........!")
     comp_choice = random.choice(pc_choice_rand)
     if comp_choice == "snake":
@@ -26,7 +26,7 @@ def gamer_choose_water():
     elif comp_choice == "gun":
         print("YOU WIN ! you destroyed the computer's Gun")
 def gamer_choose_gun():
-    print("you had choosed Gun")
+    print("you had choosed Gun 🔫")
     print("Please wait ! computer is thinking..........!")
     comp_choice = random.choice(pc_choice_rand)
     if comp_choice == "snake":
@@ -40,7 +40,7 @@ def gamer_choose_gun():
 # Main program here :
 
 print("==================== [[ SNAKE - WATER - GUN ]] =======================")
-usr_choice = input(str("you have 3 choices available......\n==> press s for snake\n==> press w for water\n==> press g for gun\n==> press e for exit the game and show score\n==> press any key to start the game\n"))
+usr_choice = input(str("you have 3 choices available......\n==> press s for snake 🐍\n==> press w for water🌊\n==> press g for gun🔫\n==> press e for exit⏱ the game and show score\n==> press any key to start the game\n"))
 while usr_choice != "e":
     gamer_choice = str(input(("\nselect your choice between snake,water & gun or press e to exit the game\n")))
     if gamer_choice=="s":
@@ -54,10 +54,10 @@ while usr_choice != "e":
         if ex_conf == "n":
             continue
         else:
-            print("\nThanks for your time...\nexiting the game..")
+            print("\n🐻__Thanks for your time__🐻\nexiting the game..⏱")
             exit()
     else:
-        print("Please choose between the provided options only")
+        print("Please choose between the provided options only 😕")
 
 else:
-    print("\nThanks for your time\nexiting the game.....\n")
+    print("\n🐻__Thanks for your time__🐻\nexiting the game.....⏱\n")
